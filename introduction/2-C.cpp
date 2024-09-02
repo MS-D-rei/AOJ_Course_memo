5e8e80402003a58c7cae6920c3c1c1b80962e24f
MS_D_rei <msmdmission1111@gmail.com>
2024-09-02 14:16:20 +0900

    Modified: 2-C answer more versatile

--- introduction/2-C.cpp
+++ introduction/2-C.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
 
+// int main() {
+//     int a{};
+//     int b{};
+//     int c{};
+//
+//     std::cin >> a >> b >> c;
+//
+//     if (a > b) {
+//         int temp{a};
+//         a = b;
+//         b = temp;
+//     }
+//     if (b > c) {
+//         int temp{b};
+//         b = c;
+//         c = temp;
+//     }
+//     if (a > b) {
+//         int temp{a};
+//         a = b;
+//         b = temp;
+//     }
+//
+//     std::cout << a << " " << b << " " << c << '\n';
+//
+//     return 0;
+// }
+
+// void print_array(int array[], int size) {
+//     for (int i{0}; i < size; ++i) {
+//         std::cout << array[i] << " ";
+//     }
+//     std::cout << '\n';
+// }
+
 int main() {
     int a{};
     int b{};
@@ -7,23 +42,20 @@ int main() {
 
     std::cin >> a >> b >> c;
 
-    if (a > b) {
-        int temp{a};
-        a = b;
-        b = temp;
-    }
-    if (b > c) {
-        int temp{b};
-        b = c;
-        c = temp;
-    }
-    if (a > b) {
-        int temp{a};
-        a = b;
-        b = temp;
+    int array[3]{a, b, c};
+    int size{sizeof(array) / sizeof(array[0])};
+
+    for (int i{size}; i > 1; --i ) {
+        for (int j{0}; j < i - 1; ++j) {
+           if (array[j] > array[j + 1]) {
+               int temp{array[j]};
+               array[j] = array[j + 1];
+               array[j + 1] = temp;
+           }
+        }
     }
 
-    std::cout << a << " " << b << " " << c << '\n';
+    std::cout << array[0] << " " << array[1] << " " << array[2] << '\n';
 
     return 0;
 }
